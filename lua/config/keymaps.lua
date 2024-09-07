ab58cd0504d354fcd9c50c5b927e67222857d64a
KevsterAmp <kevinamparado13@gmail.com>
2024-09-07 16:49:30 +0800

    add keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,8 +2,11 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-
 -- remove <cmd>Lazy<CR> init keymap
 vim.keymap.del("n", "<leader>l")
 require("harpoon").setup() -- then add harpoon
-vim.keymap.set("n", "<leader>l", function() require("harpoon"):list():select(3) end)
+vim.keymap.set("n", "<leader>l", function()
+  require("harpoon"):list():select(3)
+end)
+
+vim.keymap.set("i", "jk", "<Esc>")
