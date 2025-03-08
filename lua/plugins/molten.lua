e822b462717c553a45735275b70824ba41fa6ca8
KevsterAmp <kevinamparado13@gmail.com>
2025-03-08 15:05:43 +0800

    formatter changes on molten

--- lua/plugins/molten.lua
+++ lua/plugins/molten.lua
@@ -5,17 +5,17 @@ return {
   init = function()
     -- this is an example, not a default. Please see the readme for more configuration options
     vim.g.molten_output_win_max_height = 12
-    vim.g.molten_virt_text_output = true
-    vim.g.molten_auto_open_output	 = false
+    vim.g.molten_virt_text_output      = true
+    vim.g.molten_auto_open_output      = false
 
     vim.keymap.set("n", "<leader>mi", ":MoltenInit<CR>", { silent = true, desc = "Initialize the plugin" })
     vim.keymap.set("n", "<leader>e", ":MoltenEvaluateOperator<CR>", { silent = true, desc = "run operator selection" })
     vim.keymap.set("n", "<leader>rl", ":MoltenEvaluateLine<CR>", { silent = true, desc = "evaluate line" })
     vim.keymap.set("n", "<leader>rr", ":MoltenReevaluateCell<CR>", { silent = true, desc = "re-evaluate cell" })
-    vim.keymap.set("v", "<leader>r", ":<C-u>MoltenEvaluateVisual<CR>gv", { silent = true, desc = "evaluate visual selection" })
+    vim.keymap.set("v", "<leader>r", ":<C-u>MoltenEvaluateVisual<CR>gv",
+      { silent = true, desc = "evaluate visual selection" })
     vim.keymap.set("n", "<leader>rd", ":MoltenDelete<CR>", { silent = true, desc = "molten delete cell" })
     vim.keymap.set("n", "<leader>oh", ":MoltenHideOutput<CR>", { silent = true, desc = "hide output" })
     vim.keymap.set("n", "<leader>os", ":noautocmd MoltenEnterOutput<CR>", { silent = true, desc = "show/enter output" })
-
   end,
 }
