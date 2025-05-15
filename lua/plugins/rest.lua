4b902f3203caf3f693180cce32367df3c04aa1d1
KevsterAmp <kevinamparado13@gmail.com>
2025-05-15 14:52:51 +0800

    add run keybind on molten

--- lua/plugins/rest.lua
+++ lua/plugins/rest.lua
@@ -6,5 +6,8 @@ return {
       opts.ensure_installed = opts.ensure_installed or {}
       table.insert(opts.ensure_installed, "http")
     end,
+
+    vim.keymap.set("n", "<leader>Rr", ":Rest run<CR>", { silent = true, desc = "Run the current command" })
+
   }
 }
